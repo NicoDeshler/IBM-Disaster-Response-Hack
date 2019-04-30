6c951375347b011527e2d016d47fd63e2a739353
Justin Wong <justinryanwong@berkeley.edu>
2019-04-29 23:51:17 -0700

    snaping small distances functional

--- processImages.js
+++ processImages.js
@@ -8,69 +8,67 @@ var polylines = [];
 var snappedCoordinates = [];
 
 function initialize() {
-  var mapOptions = {
-    zoom: 17,
-    center: {lat: 37.8716, lng: -122.2727}
-  };
-  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var mapOptions = {
+        zoom: 17,
+        center: {lat: 37.8716, lng: -122.2727}
+    };
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  // Adds a Places search box. Searching for a place will center the map on that
-  // location.
-  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(
-      document.getElementById('bar'));
-  var autocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('autoc'));
-  autocomplete.bindTo('bounds', map);
-  autocomplete.addListener('place_changed', function() {
-    var place = autocomplete.getPlace();
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
-  });
+    // Adds a Places search box. Searching for a place will center the map on that
+    // location.
+    map.controls[google.maps.ControlPosition.RIGHT_TOP].push(document.getElementById('bar'));
+    var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autoc'));
 
+    autocomplete.bindTo('bounds', map);
+    autocomplete.addListener('place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+    });
 
 
 
 
-  // Enables the polyline drawing control. Click on the map to start drawing a
-  // polyline. Each click will add a new vertice. Double-click to stop drawing.
-  drawingManager = new google.maps.drawing.DrawingManager({
+
+    // Enables the polyline drawing control. Click on the map to start drawing a
+    // polyline. Each click will add a new vertice. Double-click to stop drawing.
+    drawingManager = new google.maps.drawing.DrawingManager({
     drawingMode: google.maps.drawing.OverlayType.POLYLINE,
     drawingControl: true,
     drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-        google.maps.drawing.OverlayType.POLYLINE
-      ]
-    },
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [google.maps.drawing.OverlayType.POLYLINE]
+        },
     polylineOptions: {
-      strokeColor: '#0066ff',
-      strokeWeight: 2
-    }
-  });
-  drawingManager.setMap(map);
-
-  // Snap-to-road when the polyline is completed.
-  drawingManager.addListener('polylinecomplete', function(poly) {
-    var path = poly.getPath();
-    polylines.push(poly);
-    placeIdArray = [];
-    runSnapToRoad(path);
-  });
-
-  // Clear button. Click to remove all polylines.
-  var clearBtn = document.getElementById("clear")
-  clearBtn.addEventListener("click", function(ev) {
-     for (var i = 0; i < polylines.length; ++i) {
-           polylines[i].setMap(null);
-         }
-     polylines = [];
-     ev.preventDefault();
-     return false;
+        strokeColor: '#0066ff',
+        strokeWeight: 2
+        }
     });
+    drawingManager.setMap(map);
+
+    // Snap-to-road when the polyline is completed.
+    drawingManager.addListener('polylinecomplete', function(poly) {
+        var path = poly.getPath();
+        polylines.push(poly);
+        placeIdArray = [];
+        runSnapToRoad(path);
+      });
+
+    // Clear button. Click to remove all polylines.
+    var clearBtn = document.getElementById("clear")
+    clearBtn.addEventListener("click", function(ev) {
+        for (var i = 0; i < polylines.length; ++i) {
+            polylines[i].setMap(null);
+        }
+        polylines = [];
+        ev.preventDefault();
+        return false;
+    });
+    //End of Initialize
 }
 
 // Snap a user-created polyline to roads and draw the snapped path
@@ -94,23 +92,7 @@ function runSnapToRoad(path) {
   console.log("inside runSnapToRoad:", data);
     processSnapToRoadResponse(data);
     drawSnappedPolyline();
-    getAndDrawSpeedLimits();
   });
-
-//  $.ajax({
-//      url: 'https://roads.googleapis.com/v1/snapToRoads?path=' + pathValues.join('|') + '&key=AIzaSyA1JWR3ohBFQ_P7F5eSMSJb0dwV9PbB3pA&interpolate=true', //true',
-//      crossDomain: true,
-//      dataType: 'jsonp'
-//    }).done(function(data) {
-//              // drawSnappedPolyline colors coordinates in "snappedCoordinates" yellow
-//              // snappedCoordinates are processed in processSnapToRoadResponse
-//              console.log("Inside ajax call:" , data);
-//                processSnapToRoadResponse(data);
-//                drawSnappedPolyline();
-//                getAndDrawSpeedLimits();
-//              });
-
-
 
 
 }
@@ -139,76 +121,6 @@ function drawSnappedPolyline() {
 
   snappedPolyline.setMap(map);
   polylines.push(snappedPolyline);
-}
-
-// Gets speed limits (for 100 segments at a time) and draws a polyline
-// color-coded by speed limit. Must be called after processing snap-to-road
-// response.
-function getAndDrawSpeedLimits() {
-  for (var i = 0; i <= placeIdArray.length / 100; i++) {
-    // Ensure that no query exceeds the max 100 placeID limit.
-    var start = i * 100;
-    var end = Math.min((i + 1) * 100 - 1, placeIdArray.length);
-
-    drawSpeedLimits(start, end);
-  }
-}
-
-// Gets speed limits for a 100-segment path and draws a polyline color-coded by
-// speed limit. Must be called after processing snap-to-road response.
-function drawSpeedLimits(start, end) {
-    var placeIdQuery = '';
-    for (var i = start; i < end; i++) {
-      placeIdQuery += '&placeId=' + placeIdArray[i];
-    }
-
-//    processSpeedLimitResponse(path)
-    $.get('https://roads.googleapis.com/v1/speedLimits',
-        'key=' + apiKey + placeIdQuery,
-        function(speedData) {
-          processSpeedLimitResponse(speedData, start);
-        }
-    );
-}
-
-// Draw a polyline segment (up to 100 road segments) color-coded by speed limit.
-function processSpeedLimitResponse(speedData, start) {
-  var end = start + speedData.speedLimits.length;
-  for (var i = 0; i < speedData.speedLimits.length - 1; i++) {
-    var speedLimit = speedData.speedLimits[i].speedLimit;
-    var color = getColorForSpeed(speedLimit);
-
-    // Take two points for a single-segment polyline.
-    var coords = snappedCoordinates.slice(start + i, start + i + 2);
-
-    var snappedPolyline = new google.maps.Polyline({
-      path: coords,
-      strokeColor: color,
-      strokeWeight: 6
-    });
-    snappedPolyline.setMap(map);
-    polylines.push(snappedPolyline);
-  }
-}
-
-function getColorForSpeed(speed_kph) {
-//    return 'red';
-  if (speed_kph <= 40) {
-    return 'purple';
-  }
-  if (speed_kph <= 50) {
-    return 'blue';
-  }
-  if (speed_kph <= 60) {
-    return 'green';
-  }
-  if (speed_kph <= 80) {
-    return 'yellow';
-  }
-  if (speed_kph <= 100) {
-    return 'orange';
-  }
-  return 'red';
 }
 
 
